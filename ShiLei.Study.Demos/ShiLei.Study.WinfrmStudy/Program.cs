ce443192654b7437be8d9724bfa288ef05d185bd
shilei <shilei@47.98.229.178>
2021-04-16 17:37:51 +0800

    日志:委托和多播委托

--- ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/Program.cs
+++ ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/Program.cs
@@ -16,7 +16,7 @@ namespace ShiLei.Study.WinfrmStudy
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmCalculation());
         }
     }
 }
