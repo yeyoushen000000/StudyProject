5ca82df8643f57b2390f1c7cadd31a5bab9cda1a
shilei <shilei@47.98.229.178>
2021-05-08 14:17:48 +0800

    日志： 人员：石磊 内容：学习demo 删除文件  更新代码

---
+++ ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/frmCalculation.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace ShiLei.Study.WinfrmStudy
+{
+    partial class frmCalculation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(420, 77);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 28);
+            this.textBox6.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(262, 77);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 31);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "等于";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 195);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "结果：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(204, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(593, 151);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 31);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "多播委托计算";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmCalculation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox1);
+            this.Name = "frmCalculation";
+            this.Text = "利用委托计算加减乘除";
+            this.Load += new System.EventHandler(this.frmCalculation_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/frmCalculation.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShiLei.Study.WinfrmStudy
+{
+    public partial class frmCalculation : Form
+    {
+
+
+        //声明一个委托
+        public delegate int calculate<in T>(T num1, T num2);
+        //创建一个委托对象
+        calculate<int> calMethod;
+
+        public frmCalculation()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+            int number1 = Convert.ToInt32(textBox1.Text) ;
+            int number2 = Convert.ToInt32(textBox6.Text);
+            calMethod(1,2);
+
+        }
+
+
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiple(int a, int b)
+        {
+            return a * b;
+        }
+        public int Devision(int a, int b)
+        {
+            return a / b;
+        }
+
+        private void frmCalculation_Load(object sender, EventArgs e)
+        {
+           
+           
+        }
+
+        //给委托赋值
+        private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+            string calCharacter = comboBox1.SelectedValue.ToString();
+
+            switch (calCharacter)
+            {
+                case "+":
+                    calMethod = Add;
+                    break;
+                case "-":
+                    calMethod = Subtraction;
+                    break;
+                case "*":
+                    calMethod = Multiple;
+                    break;
+                case "/":
+                    calMethod = Devision;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+   
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Func<int, int> action = null;
+            //使用+= 或 -= 来增加或移除委托实例
+            action += First;
+            action += Cal;
+            action += Three;
+            action -= Cal;
+            //如果有返回值的话，返回的是最后一次执行的结果
+            int a = action(5);
+            Console.WriteLine(a);
+        }
+
+
+
+
+        public int First(int a)
+        {
+            Console.WriteLine($"第{1}次执行" + a); return a + 1;
+        }
+
+        public int Three(int a)
+        {
+            Console.WriteLine($"第{1}次执行" + a); return a + 1;
+        }
+
+
+
+        private int Cal(int num)
+        {
+            return num * num;
+        }
+    }
+
+
+
+}
