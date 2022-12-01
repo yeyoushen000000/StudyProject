aef455272d41011d55c370ce9ed72f4d492e60a6
shilei <shilei@47.98.229.178>
2022-12-01 15:02:13 +0800

    日志 更新部分代码

--- ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/frmDelegate.Designer.cs
+++ ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/frmDelegate.Designer.cs
@@ -29,108 +29,119 @@ namespace ShiLei.Study.WinfrmStudy
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // groupBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(139, 77);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 28);
-            this.textBox1.TabIndex = 4;
-            // 
-            // textBox6
-            // 
-            this.textBox6.Location = new System.Drawing.Point(420, 77);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 28);
-            this.textBox6.TabIndex = 9;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "+",
-            "-",
-            "*",
-            "/"});
-            this.comboBox1.Location = new System.Drawing.Point(262, 77);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 26);
-            this.comboBox1.TabIndex = 10;
-            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(711, 186);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "功能区";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(593, 79);
+            this.button1.Location = new System.Drawing.Point(16, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 31);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "等于";
+            this.button1.Size = new System.Drawing.Size(186, 26);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "绑定委托加减乘除";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label1
+            // groupBox2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 195);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
-            this.label1.TabIndex = 12;
-            this.label1.Text = "结果：";
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 186);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(711, 189);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "结果展示";
             // 
-            // label2
+            // richTextBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(204, 195);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 18);
-            this.label2.TabIndex = 13;
-            this.label2.Text = "label2";
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 21);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(705, 165);
+            this.richTextBox1.TabIndex = 23;
+            this.richTextBox1.Text = "";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(593, 151);
+            this.button2.Location = new System.Drawing.Point(16, 79);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 31);
-            this.button2.TabIndex = 14;
+            this.button2.Size = new System.Drawing.Size(186, 26);
+            this.button2.TabIndex = 21;
             this.button2.Text = "多播委托计算";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // frmCalculation
+            // button3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.button3.Location = new System.Drawing.Point(16, 125);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(186, 26);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "利用系统委托计算";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(272, 33);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(186, 26);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "利用事件订阅方法";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // frmDelegate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox1);
-            this.Name = "frmCalculation";
+            this.ClientSize = new System.Drawing.Size(711, 375);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmDelegate";
             this.Text = "利用委托计算加减乘除";
             this.Load += new System.EventHandler(this.frmCalculation_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
     }
 }
--- ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/frmDelegate.cs
+++ ShiLei.Study.Demos/ShiLei.Study.WinfrmStudy/frmDelegate.cs
@@ -12,33 +12,70 @@ namespace ShiLei.Study.WinfrmStudy
 {
     public partial class frmDelegate : Form
     {
+        //1：委托是一种类型，但是它的参数是固定返回值和固定参数列表的方法
+        //2：调用委托+=表示注册方法，-=表示取消注册方法，执行的时候是按照链式执行的，也就是一个委托链条上的方法全部都会执行
+        //3：委托调用前，必须先声明，否则不可以执行
+        //4：系统委托无返回值Action，有返回值Func<T,T>
+        //5：自定义的委托可以不赋初值，但系统委托Func和Action必须赋初值（也就是将一个方法赋值给系统委托）
+        //6：事件event同样可以+=和-=进行注册和取消注册,shijia
+        //
+        //
+        //
+        //
 
 
-        //声明一个委托
+
+
+        //声明一个委托，委托使用前必须要先声明，类似于类的声明总要有个Class关键字和{}体
         public delegate int calculate<in T>(T num1, T num2);
-        //创建一个委托对象
+        //创建一个委托对象，相当于new 了一个类的对象
         calculate<int> calMethod;
+
+        //声明一个委托，用于计算平方
+        public delegate int SquareCalculateHandler(int num);
+        //创建了一个委托对象
+        public SquareCalculateHandler squareCalculate;
+
+        //创建一个事件，相当于一个委托对象
+        public event SquareCalculateHandler onSquare;
+
+
+
 
         public frmDelegate()
         {
             InitializeComponent();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void frmCalculation_Load(object sender, EventArgs e)
         {
 
+
         }
+
+        #region 委托赋值-进行加减乘除
 
         private void button1_Click(object sender, EventArgs e)
         {
-          
-            int number1 = Convert.ToInt32(textBox1.Text) ;
-            int number2 = Convert.ToInt32(textBox6.Text);
-            calMethod(1,2);
+            richTextBox1.Text = string.Empty;
+            int a = 15, b = 5;
+            calMethod = Add;
+            int result = calMethod(a, b);
+            richTextBox1.AppendText("\r\n" + $"a=15,b=5,委托执行，相加结果：" + result);
+            calMethod = Subtraction;
+            result = calMethod(a, b);
+            richTextBox1.AppendText("\r\n" + $"a=15,b=5,委托执行，相减结果：" + result);
+            calMethod = Multiple;
+            result = calMethod(a, b);
+            richTextBox1.AppendText("\r\n" + $"a=15,b=5,委托执行，相乘结果：" + result);
+            calMethod = Devision;
+            result = calMethod(a, b);
+            richTextBox1.AppendText("\r\n" + $"a=15,b=5,委托执行，相除结果：" + result);
+
 
         }
 
 
+        #region 加减乘除方法
 
         public int Add(int a, int b)
         {
@@ -57,73 +94,108 @@ namespace ShiLei.Study.WinfrmStudy
         {
             return a / b;
         }
-
-        private void frmCalculation_Load(object sender, EventArgs e)
-        {
-           
-           
-        }
-
-        //给委托赋值
-        private void comboBox1_DropDown(object sender, EventArgs e)
-        {
-            string calCharacter = comboBox1.SelectedValue.ToString();
-
-            switch (calCharacter)
-            {
-                case "+":
-                    calMethod = Add;
-                    break;
-                case "-":
-                    calMethod = Subtraction;
-                    break;
-                case "*":
-                    calMethod = Multiple;
-                    break;
-                case "/":
-                    calMethod = Devision;
-                    break;
-                default:
-                    break;
-            }
-        }
-
-
-   
-       
-
+        #endregion
+        #endregion
+  
+        
+        #region -=和+=进行多播委托操作,系统一次性执行完毕所有委托
         private void button2_Click(object sender, EventArgs e)
         {
-            Func<int, int> action = null;
             //使用+= 或 -= 来增加或移除委托实例
-            action += First;
-            action += Cal;
-            action += Three;
-            action -= Cal;
+            squareCalculate += First;
+            squareCalculate += Second;
+            squareCalculate += Three;
+            squareCalculate += Square;
+
             //如果有返回值的话，返回的是最后一次执行的结果
-            int a = action(5);
-            Console.WriteLine(a);
+            int a = squareCalculate(5);
+            richTextBox1.AppendText("\r\n" + $"委托+=符号执行完毕,输入为5，平方结果值为:" + a);
+
+
+            squareCalculate -= Square;
+            richTextBox1.AppendText("\r\n" + $"执行-=Square方法");
+
+            int b = squareCalculate(6);
+            richTextBox1.AppendText("\r\n" + $"委托-=符号执行完毕,输入为6，结果值为:" + b);
         }
+        #endregion
+   
 
-
-
-
+        #region 委托执行方法链
         public int First(int a)
         {
-            Console.WriteLine($"第{1}次执行" + a); return a + 1;
+            richTextBox1.AppendText("\r\n" + $"执行方法名：First");
+            return a + 1;
+        }
+
+        public int Second(int a)
+        {
+            richTextBox1.AppendText("\r\n" + $"执行方法名：Second");
+            return a + 1;
         }
 
         public int Three(int a)
         {
-            Console.WriteLine($"第{1}次执行" + a); return a + 1;
+            richTextBox1.AppendText("\r\n" + $"执行方法名：Three");
+            return a + 1;
         }
 
-
-
-        private int Cal(int num)
+        private int Square(int num)
         {
+            richTextBox1.AppendText("\r\n" + $"执行方法名：Square");
             return num * num;
         }
+
+        #endregion
+
+
+        #region 利用系统委托Action和Func进行计算，
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Func<int, int> action1;
+            action1 = First;
+            action1 += Second;
+            action1 += Three;
+            action1 += Square;
+            int a = action1(10);
+            richTextBox1.AppendText("\r\n" + $"执行系统委托Func,输入为10，平方结果值为:" + a);
+        }
+        #endregion
+
+
+
+        #region 利用事件订阅方法，然后执行方法
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //
+            onSquare += OnThreeSquare;
+            richTextBox1.AppendText("\r\n" + $"事件绑定方法：OnThreeSquare");
+            onSquare += OnQuartSquare;
+            richTextBox1.AppendText("\r\n" + $"事件绑定方法：OnQuartSquare");
+
+            if (onSquare != null)
+            {
+                int b = onSquare(3);
+                richTextBox1.AppendText("\r\n" + $"onSquare事件执行，输入值为3，结果为：" + b);
+            }
+        }
+
+        public int OnThreeSquare(int num)
+        {
+            richTextBox1.AppendText("\r\n" + $"执行方法名：OnThreeSquare");
+
+            return num * num * num;
+        }
+
+        public int OnQuartSquare(int num)
+        {
+            richTextBox1.AppendText("\r\n" + $"执行方法名：OnQuartSquare");
+
+            return num * num * num * num;
+        }
+        #endregion
+
+
     }
 
 
